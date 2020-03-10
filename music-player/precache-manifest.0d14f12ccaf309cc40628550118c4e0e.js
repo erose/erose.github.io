b659c3446903b5e268724593570ddaf9ae0cdba9
Eli Rose <really.eli@gmail.com>
2020-03-10 15:17:44 -0400

    Deploy music player.

---
+++ music-player/precache-manifest.0d14f12ccaf309cc40628550118c4e0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d22377d9f2fccbcde0203ac6971df1c",
+    "url": "/music-player/index.html"
+  },
+  {
+    "revision": "9a4decd53422957e3aad",
+    "url": "/music-player/static/css/main.5b9cb902.chunk.css"
+  },
+  {
+    "revision": "c6df14ab54053878078e",
+    "url": "/music-player/static/js/2.284c5281.chunk.js"
+  },
+  {
+    "revision": "1916028a505359dee8e205a38b7d49e3",
+    "url": "/music-player/static/js/2.284c5281.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a4decd53422957e3aad",
+    "url": "/music-player/static/js/main.82b77848.chunk.js"
+  },
+  {
+    "revision": "c405d3bacb8d6bbbdbc8",
+    "url": "/music-player/static/js/runtime-main.d90a06c0.js"
+  },
+  {
+    "revision": "2f27f0451cef49811159185acae00b47",
+    "url": "/music-player/static/media/spinner.2f27f045.ico"
+  }
+]);
