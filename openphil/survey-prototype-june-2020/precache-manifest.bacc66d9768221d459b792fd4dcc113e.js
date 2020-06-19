2d8377582bf7d7dcec7a7593de6b8ced246569d7
Eli Rose <really.eli@gmail.com>
2020-06-19 10:05:55 -0400

    Add survey question prototype for OpenPhil.

---
+++ openphil/survey-prototype-june-2020/precache-manifest.bacc66d9768221d459b792fd4dcc113e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df8118286e9da6fa1a6df8c8fb81150f",
+    "url": "/openphil/survey-prototype-june-2020/index.html"
+  },
+  {
+    "revision": "7a5d10a936fd4242e62d",
+    "url": "/openphil/survey-prototype-june-2020/static/css/main.6f8b8afa.chunk.css"
+  },
+  {
+    "revision": "8549d474e9e09c33fee6",
+    "url": "/openphil/survey-prototype-june-2020/static/js/2.669ad919.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/openphil/survey-prototype-june-2020/static/js/2.669ad919.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a5d10a936fd4242e62d",
+    "url": "/openphil/survey-prototype-june-2020/static/js/main.23205496.chunk.js"
+  },
+  {
+    "revision": "d15d8b3ffb3ca6ecb8b5",
+    "url": "/openphil/survey-prototype-june-2020/static/js/runtime-main.c793a3d5.js"
+  }
+]);
